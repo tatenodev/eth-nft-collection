eda66064d4febd8c23fb69f32c769e94f1f643bb
tatenodev <gd28.c.tateno@gmail.com>
2022-09-06 15:22:22 +0900

    wip

--- nft-collection-starter-project/src/App.tsx
+++ nft-collection-starter-project/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+// Constantsを宣言する: constとは値書き換えを禁止した変数を宣言する方法です。
+const TWITTER_HANDLE = "tatenodev";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const OPENSEA_LINK = "";
+const TOTAL_MINT_COUNT = 50;
+
+const App = () => {
+  // renderNotConnectedContainer メソッドを定義します。
+  const renderNotConnectedContainer = () => (
+    <button className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">My NFT Collection</p>
+          <p className="sub-text">あなただけの特別な NFT を Mint しよう💫</p>
+          {renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built on @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
