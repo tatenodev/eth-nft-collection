5533eab4be0627acc39f8dbaa3dac1dce79b593c
tatenodev <gd28.c.tateno@gmail.com>
2022-09-06 13:07:17 +0900

    重複削除

--- epic-nfts/contracts/PassNFT.sol
+++ epic-nfts/contracts/PassNFT.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
 
 import { Base64 } from "./lib/Base64.sol";
