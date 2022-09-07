15ed5d432590675d6c380b3b60205a638dfa3ac4
tatenodev <gd28.c.tateno@gmail.com>
2022-09-07 18:22:13 +0900

    add scripts

---
+++ epic-nfts/scripts/run_mint.js
@@ -0,0 +1,23 @@
+const main = async () => {
+  const nftContractFactory = await hre.ethers.getContractFactory("Web3Mint");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contract deployed to: ", nftContract.address);
+
+  let txn = await nftContract.mintIpfsNFT("poker", "bafybeihvxvr7gmv7gei3q3kcoaqx2wjw6aoiqj2bfmj4o7jczhqbovs7ii");
+  await txn.wait();
+  let returnedTokenUri = await nftContract.tokenURI(0);
+  console.log("tokenURI:", returnedTokenUri);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+runMain();
