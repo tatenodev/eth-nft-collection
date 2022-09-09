d42426a6e86afe4a4431f071c300666e8452f17e
tatenodev <gd28.c.tateno@gmail.com>
2022-09-09 12:16:55 +0900

    add deploy_game.js

---
+++ epic-nfts/scripts/deploy_game.js
@@ -0,0 +1,42 @@
+const main = async () => {
+  const gameContractFactory = await hre.ethers.getContractFactory("MyEpicGame");
+  const gameContract = await gameContractFactory.deploy(
+    ["ZORO", "NAMI", "USOPP"],
+    [
+      "https://i.imgur.com/TZEhCTX.png",
+      "https://i.imgur.com/WVAaMPA.png",
+      "https://i.imgur.com/pCMZeiM.png",
+    ],
+    [100, 200, 300],
+    [100, 50, 25]
+  )
+  await gameContract.deployed();
+
+  console.log("Contract deployed to: ", gameContract.address);
+
+  let txn = await gameContract.mintCharacterNFT(0);
+  await txn.wait();
+  console.log("Minted NFT #1");
+
+  txn = await gameContract.mintCharacterNFT(1);
+  await txn.wait();
+  console.log("Minted NFT #2");
+
+  txn = await gameContract.mintCharacterNFT(2);
+  await txn.wait();
+  console.log("Minted NFT #3");
+
+  console.log("Done deploying and minting!");
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
+
+runMain();
