2365f6719d1df6c3f831009f67e95035ef979821
tatenodev <gd28.c.tateno@gmail.com>
2022-09-12 13:31:40 +0900

    add connect button

--- nft-game-project/src/App.tsx
+++ nft-game-project/src/App.tsx
@@ -1,9 +1,48 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const TWITTER_HANDLE = "tatenodev";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState<string | null>(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) return console.log("Make sure you have MetaMask");
+
+      console.log("We have the ethereum object", ethereum);
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("account:", account);
+        setCurrentAccount(account);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) return alert("Get MetaMask!");
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
@@ -12,6 +51,13 @@ const App = () => {
           <p className="sub-text">プレイヤーと協力してボスを倒そう✨</p>
           <div className="connect-wallet-container">
             <img src="https://i.imgur.com/TXBQ4cC.png" alt="LUFFY" />
+            <button
+              type="button"
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started
+            </button>
           </div>
         </div>
         <div className="footer-container">
